b07b5f0a5c13cbd5c9e2025ed067af105ded424a
Luke M Craig <lukemcduffiecraig@gmail.com>
2018-03-28 23:07:26 -0400

    added arguments

--- NegativeHarmonizer.py
+++ NegativeHarmonizer.py
@@ -1,3 +1,4 @@
+import argparse
 import midi
 
 
@@ -10,25 +11,37 @@ def mirror_note_over_axis(note, axis):
     return int(axis + original_note_distance)
 
 
-def invert_tonality(pattern, tonic):
+def invert_tonality(pattern, tonic, ignored_channels):
     # print('tonic ' + str(tonic))
     mirror_axis = get_mirror_axis(tonic)
     # print('axis ' + str(mirror_axis))
     for track in pattern:
         for note in track:
             if type(note) is midi.NoteOnEvent or type(note) is midi.NoteOffEvent:
-                # print('original note ' + str(note.data[0]))
-                mirrored_note = mirror_note_over_axis(note.data[0], mirror_axis)
-                # print('mirrored_note ' + str(mirrored_note))
-                note.data[0] = mirrored_note
+                if note.channel not in ignored_channels:
+                    # print('original note ' + str(note.data[0]))
+                    mirrored_note = mirror_note_over_axis(note.data[0], mirror_axis)
+                    # print('mirrored_note ' + str(mirrored_note))
+                    note.data[0] = mirrored_note
 
 
-def main(input_file, tonic=midi.C_5):
+def main(input_file, tonic=midi.C_5, ignored_channels=[]):
     extension = ".mid"
     pattern = midi.read_midifile(input_file + extension)
 
-    invert_tonality(pattern, tonic)
+    invert_tonality(pattern, tonic, ignored_channels)
     midi.write_midifile(input_file + "_negative" + extension, pattern)
 
 
-main(input_file="midi_files/fugue-c-major", tonic=midi.C_5)
+parser = argparse.ArgumentParser(description='Negative Harmonize a midi file.')
+
+parser.add_argument('file', metavar='f',
+                    help='the input midi file (with no extension .mid)')
+parser.add_argument('--tonic', type=int, default=55,
+                    help='the tonic')
+parser.add_argument('--ignore', type=list, nargs="+", default=[],
+                    help='the midi channels to ignore (usually 9)')
+
+args = parser.parse_args()
+
+main(input_file=args.file, tonic=args.tonic, ignored_channels=args.ignore)
