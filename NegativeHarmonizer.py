ee07e5bdd87d052a9b16c48fae45a0bc1afe773a
Luke M Craig <lukemcduffiecraig@gmail.com>
2019-02-11 23:12:16 -0500

    finished converting to python3

--- NegativeHarmonizer.py
+++ NegativeHarmonizer.py
@@ -14,81 +14,82 @@ def mirror_note_over_axis(note, axis):
     return int(axis + original_note_distance)
 
 
-def invert_tonality(pattern, tonic, ignored_channels):
+def invert_tonality(mid, tonic, ignored_channels):
     # print('tonic ' + str(tonic))
     mirror_axis = get_mirror_axis(tonic)
     # print('axis ' + str(mirror_axis))
     original_octaves = {}
-    for i, track in enumerate(pattern.tracks):
+    for i, track in enumerate(mid.tracks):
         track_avg_notes = []
         for note in track:
-            if type(note) is mido.NoteOnEvent:
-                track_avg_notes.append(note.data[0])
+            # if type(note) is mido.NoteOnEvent:
+            if note.type == 'note_on':
+                track_avg_notes.append(note.note)
         if len(track_avg_notes) > 0:
             track_avg_note = sum(track_avg_notes) / len(track_avg_notes)
             original_octaves[i] = track_avg_note
             try:
-                track_text = track[0].text
+                track_name = track.name
             except AttributeError:
-                track_text = i
-            print(track_text, track_avg_note)
+                track_name = i
+            print(track_name, track_avg_note)
 
-    for track in pattern:
+    for track in mid.tracks:
         for note in track:
-            if type(note) is mido.NoteOnEvent or type(note) is mido.NoteOffEvent:
+            if note.type == 'note_on' or note.type == 'note_off':
                 if note.channel not in ignored_channels:
                     # print('original note ' + str(note.data[0]))
-                    mirrored_note = mirror_note_over_axis(note.data[0], mirror_axis)
+                    mirrored_note = mirror_note_over_axis(note.note, mirror_axis)
                     # print('mirrored_note ' + str(mirrored_note))
-                    note.data[0] = mirrored_note
+                    note.note = mirrored_note
     print("---")
+
     new_octaves = {}
-    for i, track in enumerate(pattern):
+    for i, track in enumerate(mid.tracks):
         track_avg_notes = []
         for note in track:
-            if type(note) is mido.NoteOnEvent:
-                track_avg_notes.append(note.data[0])
+            if note.type == 'note_on':
+                track_avg_notes.append(note.note)
         if len(track_avg_notes) > 0:
             track_avg_note = sum(track_avg_notes) / len(track_avg_notes)
             new_octaves[i] = track_avg_note
             try:
-                track_text = track[0].text
+                track_name = track.name
             except AttributeError:
-                track_text = i
-            print(track_text, track_avg_note)
+                track_name = i
+            print(track_name, track_avg_note)
 
-    for i, track in enumerate(pattern):
-        if original_octaves.has_key(i):
+    for i, track in enumerate(mid.tracks):
+        if i in original_octaves:
             notes_distance = original_octaves[i] - new_octaves[i]
             octaves_to_transpose = round(notes_distance / 12)
             for note in track:
-                if type(note) is mido.NoteOnEvent or type(note) is mido.NoteOffEvent:
+                if note.type == 'note_on' or note.type == 'note_off':
                     if note.channel not in ignored_channels:
-                        transposed_note = note.data[0] + (octaves_to_transpose * 12)
-                        note.data[0] = int(transposed_note)
+                        transposed_note = note.note + (octaves_to_transpose * 12)
+                        note.note = int(transposed_note)
 
     print("---")
-    for i, track in enumerate(pattern):
+    for i, track in enumerate(mid.tracks):
         track_avg_notes = []
         for note in track:
-            if type(note) is mido.NoteOnEvent:
-                track_avg_notes.append(note.data[0])
-        if len(track_avg_notes) > 0:
-            track_avg_note = sum(track_avg_notes) / len(track_avg_notes)
+            if note.type == 'note_on':
+                track_avg_notes.append(note.note)
+                if len(track_avg_notes) > 0:
+                    track_avg_note = sum(track_avg_notes) / len(track_avg_notes)
             try:
-                track_text = track[0].text
+                track_name = track[0].text
             except AttributeError:
-                track_text = i
-            print(track_text, track_avg_note)
+                track_name = i
+            print(track_name, track_avg_note)
 
 
-def main(input_file, tonic=72, ignored_channels=[]):
-    # 72=C5
+def main(input_file, tonic=55, ignored_channels=[]):
     root, ext = os.path.splitext(input_file)
-    pattern = mido.MidiFile(input_file)
+    mid = mido.MidiFile(input_file)
 
-    invert_tonality(pattern, tonic, ignored_channels)
-    mido.write_midifile(root + "_negative" + ext, pattern)
+    invert_tonality(mid, tonic, ignored_channels)
+    mid.save(root + "_negative" + ext)
 
 
 parser = argparse.ArgumentParser(description='Negative Harmonize a midi file.')
