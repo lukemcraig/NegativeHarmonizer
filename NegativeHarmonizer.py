a9d7eec7d2dedc20f63cde99f81774e76937948d
Luke M Craig <lukemcduffiecraig@gmail.com>
2019-02-11 23:34:23 -0500

    refactored duplicate methods out and fixed an indentation error

--- NegativeHarmonizer.py
+++ NegativeHarmonizer.py
@@ -13,51 +13,35 @@ def mirror_note_over_axis(note, axis):
     return int(axis + original_note_distance)
 
 
-def invert_tonality(mid, tonic, ignored_channels):
-    # print('tonic ' + str(tonic))
-    mirror_axis = get_mirror_axis(tonic)
-    # print('axis ' + str(mirror_axis))
-    original_octaves = {}
+def find_average_octave_of_tracks(mid):
+    octaves = {}
     for i, track in enumerate(mid.tracks):
         track_avg_notes = []
         for note in track:
-            # if type(note) is mido.NoteOnEvent:
             if note.type == 'note_on':
                 track_avg_notes.append(note.note)
         if len(track_avg_notes) > 0:
             track_avg_note = sum(track_avg_notes) / len(track_avg_notes)
-            original_octaves[i] = track_avg_note
+            octaves[i] = track_avg_note
             try:
                 track_name = track.name
             except AttributeError:
                 track_name = i
             print(track_name, track_avg_note)
+    return octaves, track_avg_note
 
+
+def mirror_all_notes(ignored_channels, mid, mirror_axis):
     for track in mid.tracks:
         for note in track:
             if note.type == 'note_on' or note.type == 'note_off':
                 if note.channel not in ignored_channels:
-                    # print('original note ' + str(note.data[0]))
                     mirrored_note = mirror_note_over_axis(note.note, mirror_axis)
-                    # print('mirrored_note ' + str(mirrored_note))
                     note.note = mirrored_note
-    print("---")
+    return
 
-    new_octaves = {}
-    for i, track in enumerate(mid.tracks):
-        track_avg_notes = []
-        for note in track:
-            if note.type == 'note_on':
-                track_avg_notes.append(note.note)
-        if len(track_avg_notes) > 0:
-            track_avg_note = sum(track_avg_notes) / len(track_avg_notes)
-            new_octaves[i] = track_avg_note
-            try:
-                track_name = track.name
-            except AttributeError:
-                track_name = i
-            print(track_name, track_avg_note)
 
+def transpose_back_to_original_octaves(ignored_channels, mid, new_octaves, original_octaves):
     for i, track in enumerate(mid.tracks):
         if i in original_octaves:
             notes_distance = original_octaves[i] - new_octaves[i]
@@ -68,19 +52,36 @@ def invert_tonality(mid, tonic, ignored_channels):
                         transposed_note = note.note + (octaves_to_transpose * 12)
                         note.note = int(transposed_note)
 
+
+def invert_tonality(mid, tonic, ignored_channels):
+    mirror_axis = get_mirror_axis(tonic)
     print("---")
+    print("original average note values:")
+    original_octaves, track_avg_note = find_average_octave_of_tracks(mid)
+
+    mirror_all_notes(ignored_channels, mid, mirror_axis)
+
+    print("---")
+    print("new average note values:")
+    new_octaves, track_avg_note = find_average_octave_of_tracks(mid)
+
+    transpose_back_to_original_octaves(ignored_channels, mid, new_octaves, original_octaves)
+
+    print("---")
+    print("adjusted average note values:")
     for i, track in enumerate(mid.tracks):
         track_avg_notes = []
         for note in track:
             if note.type == 'note_on':
                 track_avg_notes.append(note.note)
-                if len(track_avg_notes) > 0:
-                    track_avg_note = sum(track_avg_notes) / len(track_avg_notes)
+        if len(track_avg_notes) > 0:
+            track_avg_note = sum(track_avg_notes) / len(track_avg_notes)
             try:
-                track_name = track[0].text
+                track_name = track.name
             except AttributeError:
                 track_name = i
             print(track_name, track_avg_note)
+    return
 
 
 def main(input_file, tonic=55, ignored_channels=[]):
